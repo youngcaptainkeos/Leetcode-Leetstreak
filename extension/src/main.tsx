ced9ccc7cf4887cabf97f6240e6dd74c2bcf49a3
LeetStreak Developer <developer@leetstreak.com>
2026-09-13 20:36:49 +0530

    Fix React error #299 by moving script tag into body with defer and adding DOMContentLoaded fallback in main.tsx

--- extension/src/main.tsx
+++ extension/src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function renderApp() {
+  const rootElement = document.getElementById("root");
+  if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } else {
+    document.addEventListener("DOMContentLoaded", () => {
+      const el = document.getElementById("root");
+      if (el) {
+        ReactDOM.createRoot(el).render(
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        );
+      }
+    });
+  }
+}
+
+renderApp();
